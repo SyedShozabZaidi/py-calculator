b5f9b32b94aa9128667a5172dd52b42c696ce388
SyedShozabZaidi <shozthegreat@gmail.com>
2023-02-07 00:27:37 +0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,26 @@
+a = 9
+b = 1
+ans1 = a+b
+print( "the addition of 9 + 1 is 10") 
+print(ans1)
+n = 10
+m = 78
+ansnm = (n * m)
+print( "the multiplication of n and m is given")
+print(ansnm)
+g = 9
+i = 3
+# floor division
+ansgi = (g//i)
+print(ansgi)
+r = 1
+e = 2
+outputre= (r**e)
+# exponential
+print(outputre)
+print("this is the output of \"re\" ")
+# modulus
+y = 110
+z = 4444
+outputyz = (y%z)
+print(outputyz)
